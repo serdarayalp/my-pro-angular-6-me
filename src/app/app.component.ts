1254d255e444ad88da8cf45e8a924f975e5b0944
Serdar Ayalp <serdarayalp@googlemail.com>
2020-11-12 16:38:44 +0100

    Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { Model } from "./model";
+import { Model, TodoItem } from "./model";
 
 @Component({
   selector: "todo-app",
@@ -19,5 +19,11 @@ export class AppComponent {
     return this.model.items.filter(function (item) { return !item.done });
     */
     return this.model.items.filter(item => !item.done);
+  }
+
+  addItem(newItem) {
+    if (newItem != "") {
+      this.model.items.push(new TodoItem(newItem, false));
+    }
   }
 }
